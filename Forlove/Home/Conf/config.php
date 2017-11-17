0c7ea742059ffd9ee6ea5e79065b577492f1e156
wangpenglei <15649012981@163.com>
2017-11-17 10:08:39 +0800

    第一次提交

--- Forlove/Home/Conf/config.php
+++ Forlove/Home/Conf/config.php
@@ -2,10 +2,10 @@
 return array(
 	//'配置项'=>'配置值'
 	'DB_TYPE'=>'mysql',   //设置数据库类型
-	'DB_HOST'=>'localhost',//设置主机
-	'DB_NAME'=>'forlove',//设置数据库名
-	'DB_USER'=>'root',    //设置用户名
-	'DB_PWD'=>'root',        //设置密码
+	'DB_HOST'=>'47.93.217.133',//设置主机
+	'DB_NAME'=>'mylove',//设置数据库名
+	'DB_USER'=>'mylove',    //设置用户名
+	'DB_PWD'=>'3Nb7STzzST',        //设置密码
 	'DB_PORT'=>'3306',   //设置端口号
 	'DB_PREFIX'=>'ty_',  //设置表前缀
 	// 'SHOW_PAGE_TRACE' =>true,
